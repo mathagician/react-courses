5f423f6f18c8e40fca918b37d786dd812c29eecd
Harshit <dev.harshit@outlook.com>
2023-07-18 22:39:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-ReactDOM.render(<h1>Hello </h1>, document.getElementById("root"))
+/*
+
+	ReactDOM.render(<h1>Hello </h1>, document.getElementById("root"))
+
+*/
+
+const navbar = (
+	<nav>
+		<h1>Restro</h1>
+		<ul>
+			<li>Mango Juice</li>
+			<li>Apple Juice</li>
+		</ul>
+	</nav>
+)
+
+
+const root = document.getElementById("root")
+ReactDOM.render(navbar, root)
